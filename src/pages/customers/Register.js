1ee4dbf2f874adc90f7f63d554dc0dfcbc44a9bd
leandrosiq <le_kts12@hotmail.com>
2021-05-26 04:49:04 -0300

    adicionando state de loading para o button

--- src/pages/customers/Register.js
+++ src/pages/customers/Register.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { TextField, Button, CircularProgress } from '@material-ui/core';
+
+import Toasty from '../../components/Toasty';
 
 const useStyles = makeStyles(() => ({
   input: {
@@ -11,8 +12,20 @@ const useStyles = makeStyles(() => ({
     marginTop: 30
   },
   button: {
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
+  boxLoading: {
+    width: 250,
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+    gap: 15,
+    paddingTop: 15,
+  },
+  circle: {
+    position: "relative",
+    top: 10,
+  },
 }));
 
 export default function CustomersRegister() {
@@ -29,6 +42,8 @@ export default function CustomersRegister() {
     },
   });
 
+  const [openToasty, setOpenToasty] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleInputChange(event) {
     const { name, value } = event.target;
@@ -70,10 +85,18 @@ export default function CustomersRegister() {
       return setForm(newFormState);
     }
 
+    setIsLoading(true);
+    register();
+  }
+
+  function register() {
     axios.post('https://reqres.in/api/users', {
       name: form.name.value,
       job: form.job.value,
-    }).then((response) => console.log(response));
+    }).then((response) => {
+      setOpenToasty(true);
+      setIsLoading(false);
+    });
   }
 
   return (
@@ -101,9 +124,34 @@ export default function CustomersRegister() {
         variant="outlined" 
       /> <br />
 
-      <Button onClick={handleRegisterButton} className={classes.button} variant="contained" color="secondary">
-        Cadastrar
-      </Button>
+      <div className={classes.boxLoading}>
+        { 
+          isLoading ? 
+            <CircularProgress 
+              className={classes.circle} 
+              size={30} 
+              color="secondary" 
+            /> 
+          : ''
+        }
+        
+        <Button 
+          onClick={handleRegisterButton} 
+          className={classes.button} 
+          variant="contained" 
+          color="secondary"
+          disabled={isLoading}
+        >
+          { isLoading ? "Aguarde..." : "Cadastrar" }
+        </Button>
+      </div>
+      
+      <Toasty 
+        open={openToasty} 
+        severity="success" 
+        message="Cadastro realizado com sucesso!" 
+        onClose={() => setOpenToasty(false)}
+      />
     </>
   );
 }
